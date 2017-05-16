d8e0e1d3ccc0c9071dd11e546a2ae4c1468569b6
Fernando <framirez@imaginabit.com>
2017-05-16 19:32:12 +0100

    remove forced erros

--- FpCategorias.php
+++ FpCategorias.php
@@ -15,9 +15,9 @@ Author URI: http://freepress.coop
 
 
 //activate errors
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 
 if (!class_exists('FpCategorias') ){
